b933bed844fa446a109315b988cab5a797de077d
QUYQUY123 <quyhslc11@gmail.com>
2023-10-18 10:04:04 +0700

    first commit

---
+++ java01/src/java01/Helo.java
@@ -0,0 +1,34 @@
+package java01;
+
+import java.util.Scanner;
+
+public class Helo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap ho va ten:");
+		String hoVaTen = sc.nextLine();
+		System.out.println("Nhap ma sinh vien:");
+		long maSinhVien = sc.nextLong();
+		System.out.println("Nhap vao diem thi:");
+		float diemThi = sc.nextFloat();
+		System.out.println("----------------");
+		System.out.println("Ho va Ten:"+hoVaTen);
+		System.out.println("Ma Sinh vien:"+maSinhVien);
+		System.out.println("Diem Thi:"+diemThi);
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ java10/src/java10/vidu.java
@@ -0,0 +1,37 @@
+package java10;
+
+public class vidu {
+	public static void main(String[] args) {
+		int a=100;
+		int b=2;
+		
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		
+		float c= a;
+		System.out.println("a:"+c);
+		
+		float quy = 3.9f;
+		int e = (int)quy;
+		System.out.println("e:"+e);
+		// ep kieu doi tuong
+		
+		int x = new Integer(32);
+		System.out.println("x:"+x);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
